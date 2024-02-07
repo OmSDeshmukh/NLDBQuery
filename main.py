e67c0fb0eead24dadc7a60000561f1bc8c3ea31b
Om Deshmukh <omyash1111@gmail.com>
2024-02-07 16:17:51 +0530

    UPDATED file names

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ from llm_helper import get_dbs_chain
 
 st.title("Talk with database")
 
+chain = get_dbs_chain()
+
 question = st.text_input("Question")
 
 if(question):
-    chain = get_dbs_chain()
     answer = chain.run(question)
     st.header('Answer')
     st.write(answer)
-    
